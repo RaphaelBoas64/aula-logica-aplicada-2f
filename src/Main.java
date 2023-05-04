01f9da9937d5e3d7cd0214ed15930d30a2707b43
RaphaelBoas64 <raphaelboas20k23@gmail.com>
2023-05-04 08:29:48 -0300

    Atualizada aula

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int contador = 0;
+        while(contador < 10){
+
+            System.out.println("Contador = " + contador);
+            contador = contador + 1;
+        }
+        int cont = 1;
+        do {
+            System.out.println(cont);
+            cont++; // incremento
+        }while(cont <= 10);
+
+        for(int x=0; x<10; x++){
+            System.out.println(x);
+        }
+        double[] nota = new double[500];
+        nota[0] = 5.0;
+        nota[1] = 10.0;
+        nota[2] = 4.0;
+        nota[3] = 5.0;
+        nota[4] = 6.0;
+
+        for(int x = 0; x < nota.length; x++){
+            System.out.println(nota[x]);
+        }
     }
 }
